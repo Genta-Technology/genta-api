32d5755474a1699f21fd998392134596e38c78ff
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2023-11-04 22:49:24 -0700

    fix version

--- src/genta_api/__init__.py
+++ src/genta_api/__init__.py
@@ -4,7 +4,7 @@ genta-api
 Simple python interface to access models from Genta API.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 __author__ = 'Rifky Bujana bisri'
 __credits__ = 'Genta Technology'
 
